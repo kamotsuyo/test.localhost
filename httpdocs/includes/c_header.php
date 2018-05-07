76e33ee75490a60afb0ff40ecd4d59d0575b7345
kamo(mac) <kamotsuyo@gmail.com>
2018-05-07 14:21:37 +0900

    cssでパネル

---
+++ httpdocs/includes/c_header.php
@@ -0,0 +1,9 @@
+<!--meta-->
+<meta charset= "UTF-8">
+
+<!--css-->
+<link rel="stylesheet" href="reset.css"/>
+<link rel="stylesheet" href="main.css"/>
+<link rel="stylesheet" href="oc_menu.css"/>
+<!--js-->
+<script src="main.js"></script>
---
+++ httpdocs/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE HTML>
+<html lang = "ja">
+<head>
+    <!--include-->
+    <?php include( dirname( __FILE__ ) . '/includes/c_header.php' ); ?>
+
+    <title>local_mamp_test</title>
+
+</head>
+
+<body>
+    <h1>local_mamp_test</h1>
+
+    <h2>リストです!!</h2>
+    <a name="page1">page1</a>
+    <ul>
+        <li>一番上</li>
+        <li>二番目</li>
+        <li>win7で作成</li>
+        <li>win7 その２</li>
+        <li>mac 作成</li>
+
+    </ul>
+    <div class="gn">
+        <label for="panel1">開閉パネル</label>
+        <input type="checkbox" id="panel1" class="on-off" checked/>
+
+        <ul>
+            <li><a href="#1"> link1</a></li>
+            <li><a href="evernote://#hoge2"> link2</a></li>
+            <li><a href="#page1"> link3</a></li>
+            <li><a href="#"> link4</a></li>
+            <li><a href="#"> link5</a></li>
+        </ul>
+    </div>
+    <p>hogehoge</p>
+    <p>更新</p>
+    <p>GitHubウェブサイトで更新した＝＞MACで</p>
+    <a name="1">hogehoge</a>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <a name="hoge2">hoge2</a>
+</body>
